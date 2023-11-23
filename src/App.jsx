e32de87f9475f29522088f6498126c40243bafc6
Cláudio Pedro Mindouro <claudiopedrom@gmail.com>
2023-11-23 19:48:08 +0100

    test(checkbox): disable button

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [disabled, setDisabled] = useState(false);
   const [buttonColor, setButtonColor] = useState("red");
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
 
@@ -10,11 +11,12 @@ function App() {
       <button
         className={buttonColor}
         onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
       >
         Change to {newButtonColor}
       </button>
       <br />
-      <input type="checkbox" id="disable-button-checkbox" defaultChecked={false} />
+      <input type="checkbox" id="disable-button-checkbox" defaultChecked={false} onChange={(e) => setDisabled(e.target.checked)} />
       <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import { expect } from "vitest";
 
 test("button click flow", () => {
   render(<App />);
@@ -36,4 +37,12 @@ test("checkbox flow", () => {
   // check initial conditions
   expect(buttonElement).toBeEnabled();
   expect(checkboxElement).not.toBeChecked();
+
+  // click checkbox to disable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeDisabled();
+
+  // click checkbox again to re-enable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
 })
